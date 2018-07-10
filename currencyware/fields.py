7d85028b9d66b20959f7cc806127a15b54eb103f
Val Neekman <val@neekware.com>
2018-07-09 21:44:49 -0400

    clean up

--- currencyware/fields.py
+++ currencyware/fields.py
@@ -6,8 +6,8 @@ from . import defaults as defs
 class CurrencyField(models.CharField):
     """ Custom Currency Filed """
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('max_length', 2)
-        kwargs.setdefault('choices', defs.CURRENCY_CODES)
+        kwargs.setdefault('max_length', 3)
+        kwargs.setdefault('choices', defs.ALL_CURRENCY_CODES)
         super().__init__(*args, **kwargs)
 
     def get_internal_type(self):
