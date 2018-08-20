abdaff036fd391714dd2b0c036e23a2a0a8a1c0f
Val Neekman <val@neekware.com>
2018-08-19 21:24:01 -0400

    simplify command names

---
+++ currencyware/management/commands/currency.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import codecs
+import logging
+import json
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+from django.utils.translation import activate
+
+from ...models import Currency, Rate
+from ...currency import get_display
+from ... import defaults as defs
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    # Note: admin:skip
+    help = 'Load currency data'
+    path = os.path.abspath(os.path.join(os.path.realpath(__file__), '../../../', 'currency.json'))
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-p',
+            '--path',
+            dest='path',
+            default=self.path,
+            help='path to a directory for currencies file.'
+        )
+
+        parser.add_argument(
+            '--flush',
+            dest='flush',
+            default=False,
+            action='store_true',
+            help='delete all existing currencies in db'
+        )
+
+        parser.add_argument(
+            '-o',
+            '--overwrite',
+            dest='overwrite',
+            action='store_true',
+            default=False,
+            help='overwrite currencies if already found in db'
+        )
+
+    def handle(self, *args, **options):
+        verbosity = options['verbosity']
+
+        path = options['path'] or self.path
+        overwrite = options['overwrite']
+        flush = options['flush']
+        
+        if not os.path.isfile(path):
+            self.stdout.write('No currency file found at path')
+            self.stdout.write(path)
+            self.print_help("", subcommand='loadcurrency')
+            return
+
+        if flush:
+            self.stdout.write('You are about to delete all currencies from db')
+            confirm = input('Are you sure? [yes/no]: ')
+            if confirm == 'yes':
+                Currency.objects.all().delete()
+                self.stdout.write('Currencies deleted from db.')
+
+        if verbosity > 2:
+            self.stdout.write('Preparing currency file ...')
+
+        fp = codecs.open(path, encoding='utf-8')
+        self.data = json.load(fp)
+
+        activate(defs.DEFAULT_CURRENY_LANGUAGE_CODE)
+        new_count, update_count = 0, 0
+        for curr in self.data:
+            created = False
+            defaults = {
+                'code': curr.get('code'),
+                'name': get_display(curr.get('code')),
+                'number': curr.get('number', 0),
+                'symbol': curr.get('symbol', ''),
+                'unit': curr.get('unit', 2),
+                'country': ' '.join(curr.get('country', [])),
+            }
+            if overwrite:
+                instance, created = Currency.objects.get_or_create_unique(defaults, ['code'])
+            else:
+                instance = Currency.objects.get_unique_or_none(code=defaults['code'])
+                if not instance:
+                    instance, created = Currency.objects.get_or_create_unique(defaults, ['code'])
+
+            if created:
+                new_count += 1
+            elif overwrite:
+                update_count += 1
+        
+        self.stdout.write('Created {count} currenies'.format(count=new_count))
+        self.stdout.write('Updated {count} currenies'.format(count=update_count))
---
+++ currencyware/management/commands/rate.py
@@ -0,0 +1,126 @@
+import sys
+import json
+import codecs
+import logging
+import requests
+from datetime import datetime
+
+from django.conf import settings
+from django.utils import timezone
+from django.utils import translation
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+
+from toolware.utils.generic import get_days_ago
+
+from ...currency import get_display
+from ...models import Rate, Currency
+from ... import defaults as defs
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    # Translators: admin
+    help = "Fetches live rates from open exchange rates api"
+
+    OXR_URL = defs.OPEN_EXCHANGE_RATES_URL
+    OXR_KEY = defs.OPEN_EXCHANGE_RATES_API_KEY
+    LANGUAGE_CODE = getattr(settings, 'LANGUAGE_CODE', 'en')
+    
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--flush',
+            action='store_true',
+            dest='flush',
+            default=False,
+            help='Remove all rates from db'
+        )
+
+        parser.add_argument(
+            '--purge',
+            action='store',
+            dest='days',
+            type=int,
+            help='Remove rates older than x days'
+        )
+
+        parser.add_argument(
+            '-p',
+            '--poll',
+            action='store_true',
+            dest='poll',
+            default=False,
+            help='Fetch and load all rates to db'
+        )
+
+    def handle(self, *args, **options):
+        translation.activate(self.LANGUAGE_CODE)
+        self.verbosity = options['verbosity']
+        self.days = options['days']
+        self.flush = options['flush']
+        self.poll = options['poll']
+
+        if not (self.flush or self.days or self.poll):
+            self.print_help("", subcommand='loadcurrency')
+            return
+            
+        if self.flush:
+            self.flush_all()
+
+        if self.days:
+            if self.days < 2:
+                # purge only after 2 days old (compensate for UTC)
+                self.stdout.write('Purge can be done on data 2 days or older')
+                return
+            self.purge_outdated()
+
+        if self.poll:
+            self.fetch_all()
+
+    def fetch_all(self):
+
+        if self.verbosity > 2:
+            self.stdout.write('Preparing to fetch rates ...')
+
+        resp = requests.get(self.OXR_URL, params={'app_id': self.OXR_KEY, 'base': defs.BASE_CURRENY_CODE})
+        if resp.status_code != requests.codes.ok:
+            self.stdout.write('Failed to fetch rates ...')
+            self.stdout.write(resp.text)
+            return
+
+        self.data = resp.json()
+
+        new_count, update_count = 0, 0
+        updated = datetime.fromtimestamp(self.data['timestamp'], tz=timezone.utc)
+        for code, rate in self.data['rates'].items():
+            created = False
+            defaults = {
+                'code': code,
+                'date': updated,
+                'rate': rate,
+                'name': get_display(code)
+            }
+            instance, created = Rate.objects.get_or_create_unique(defaults, ['code', 'date'])
+            if created:
+                new_count += 1
+            else:
+                update_count += 1
+            
+            if not instance and self.verbosity >=3:
+                sys.stdout.write('Failed to create rate for ({code})\n'.format(code=code))
+
+        self.stdout.write('Created {count} currenies'.format(count=new_count))
+        self.stdout.write('Updated {count} currenies'.format(count=update_count))
+
+    def flush_all(self):
+        self.stdout.write('You are about to delete all rates from db')
+        confirm = input('Are you sure? [yes/no]: ')
+        if confirm == 'yes':
+            Rate.objects.all().delete()
+            self.stdout.write('Flushed rates from db.')
+    
+    def purge_outdated(self):
+        days_ago = get_days_ago(self.days)
+        Rate.objects.filter(date__lte=days_ago).delete()
+        self.stdout.write('Purged rates older than ({}) ago from db.'.format(self.days))
